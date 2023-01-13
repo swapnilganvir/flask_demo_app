d2421736aa576c40dcf41c1fadbdc4fb84cddd97
swapnilganvir <75731667+swapnilganvir@users.noreply.github.com>
2023-01-13 17:40:23 +0530

    add model.py

---
+++ model.py
@@ -0,0 +1,7 @@
+# create a function in seperate file and then call it inside app.py
+# Ex.
+# result = model.predict([[Gender, Married, ApplicantIncome, LoanAmount, Credit_History]]) 
+
+def rec_sys():
+    """model code"""
+    return 
